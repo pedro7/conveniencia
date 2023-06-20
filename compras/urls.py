782cb67bc4e56a19a88a00f7e44ff9c7c66c371c
pedro7 <pedrogabrielappel@gmail.com>
2023-06-20 17:57:45 -0300

    Add project files

--- compras/urls.py
+++ compras/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.compras, name='compras'),
     path('adicionar/', views.adicionar_produto, name='adicionar_produto'),
     path('remover/<str:codigo_barras>/', views.remover_produto, name='remover_produto'),
-    path('limpar/', views.limpar_carrinho, name='limpar_carrinho')
+    path('limpar/', views.limpar_carrinho, name='limpar_carrinho'),
+    path('finalizar/', views.finalizar, name='finalizar')
 ]
