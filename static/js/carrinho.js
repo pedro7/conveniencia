1c18127a9c3b12e15e7f249ae2d0d0a917a8440a
pedro7 <pedrogabrielappel@gmail.com>
2023-06-25 17:51:57 -0300

    Add project files

--- static/js/carrinho.js
+++ static/js/carrinho.js
@@ -3,9 +3,11 @@ var consultar_btn = document.getElementById('consultar_btn');
 
 var finalizar_modal = document.getElementById('finalizar_modal');
 var consultar_modal = document.getElementById('consultar_modal');
+var total_gasto_modal = document.getElementById('total_gasto_modal');
 
 var fechar_modal_1 = document.getElementById('fechar_modal1');
 var fechar_modal_2 = document.getElementById('fechar_modal2');
+var fechar_modal_3 = document.getElementById('fechar_modal3');
 
 finalizar_btn.addEventListener('click', function() {
     finalizar_modal.style.display = 'block';
@@ -23,11 +25,20 @@ fechar_modal_2.onclick = function() {
     consultar_modal.style.display = "none";
 };
 
+if (fechar_modal_3 != null) {
+    fechar_modal_3.onclick = function() {
+        total_gasto_modal.style.display = "none";
+    };
+}
+
 window.onclick = function(event) {
     if (event.target == finalizar_modal) {
       finalizar_modal.style.display = "none";
     }
     else if (event.target == consultar_modal) {
         consultar_modal.style.display = 'none'
+    }
+    else if (event.target == total_gasto_modal) {
+        total_gasto_modal.style.display = 'none'
     }
   };
--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,25 @@
-var modal = document.getElementsByClassName("modal")[0]
-var table = document.querySelector("table");
-var rows = table.querySelectorAll("tr");
+// var modal = document.getElementsByClassName("modal")[0]
+// var table = document.querySelector("table");
+// var rows = table.querySelectorAll("tr");
 
-var filteredRows = Array.from(rows).filter(function(rows, index) {
-  return index !== 0 && index !== rows.length - 1;
-});
+// var filteredRows = Array.from(rows).filter(function(rows, index) {
+//   return index !== 0 && index !== rows.length - 1;
+// });
 
-filteredRows.forEach(function(row) {
-  row.onclick = function() {
-    modal.style.display = "block";
-  };
-});
+// filteredRows.forEach(function(row) {
+//   row.onclick = function() {
+//     modal.style.display = "block";
+//   };
+// });
 
-var span = document.getElementsByClassName("close")[0];
+// var span = document.getElementsByClassName("close")[0];
 
-span.onclick = function() {
-  modal.style.display = "none";
-};
+// span.onclick = function() {
+//   modal.style.display = "none";
+// };
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
